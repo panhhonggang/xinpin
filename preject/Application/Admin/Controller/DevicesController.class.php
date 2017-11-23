85efa413dee0c8d88fdbfc56f74def9b34649f16
ping <chenchangping@foxmail.com>
2017-11-23 16:06:02 +0800

    全改了。妈蛋的

--- preject/Application/Admin/Controller/DevicesController.class.php
+++ preject/Application/Admin/Controller/DevicesController.class.php
@@ -36,16 +36,146 @@ class DevicesController extends Controller
     /**
      * 设备添加处理
      */
-    public function add_device()
+    public function add_device( $code=null )
     {
+        echo "<pre>";
         $devices = D('Devices');
         $code = I('device_code');
         if(!$devices->create()){
             $this->error($devices->getError(), 'show_add_device');
         }
-        if(!$devices->add_device($code)){
+        if(!$devices->add_device($value)){
             $this->error('添加失败', 'show_add_device');
         }
-        $this->success('添加成功', 'show_add_device', 100);
+        $this->success('添加成功', 'show_add_device', 3);
+    }
+
+    /**
+     * 批量上传
+     * @return [type] [description]
+     */
+    public function upload()
+    {   
+        header("Content-Type:text/html;charset=utf-8");
+        $upload = new \Think\Upload(); // 实例化上传类
+        $upload->maxSize = 3145728; // 设置附件上传大小
+        $upload->exts = array(
+            'xls',
+            'xlsx'
+        ); // 设置附件上传类
+        $upload->savePath = '/'; // 设置附件上传目录
+        // 上传文件
+        $info = $upload->uploadOne($_FILES['batch']);
+        $filename = './Uploads' . $info['savepath'] . $info['savename'];
+        $exts = $info['ext'];
+
+        if (! $info) { 
+            // 上传错误提示错误信息
+            $this->error($upload->getError());
+        } else { 
+            // 上传成功
+            $this->goods_import($filename, $exts);
+        }
+    }
+
+    public function save_import($data)
+    {   
+        $i = 0;
+        foreach ($data as $key => $val) {
+            $_POST['device_code'] = $val['A'];
+            // $datas['addtime'] = time();
+            $Devices = D('Devices'); 
+            $info = $Devices->create();
+            if($info){
+                $res = $Devices->add();
+                if (!$res) {
+                    
+                    $this->error('导入失败啦！');
+                }
+            } else {
+                $this->error('已导入' . $i . '条数据<br>' . $_POST['device_code'] . '不正确');
+            }   
+            $i ++;
+        }
+
+        $this->success($i . '条数据导入成功',U('Devices/show_add_device'));
+    }
+
+    private function getExcel($fileName, $headArr, $data)
+    {
+        vendor('PHPExcel');
+        $date = date("Y_m_d", time());
+        $fileName .= "_{$date}.xls";
+        $objPHPExcel = new \PHPExcel();
+        $objProps = $objPHPExcel->getProperties();
+        // 设置表头
+        $key = ord("A");
+        foreach ($headArr as $v) {
+            $colum = chr($key);
+            $objPHPExcel->setActiveSheetIndex(0)->setCellValue($colum . '1', $v);
+            $objPHPExcel->setActiveSheetIndex(0)->setCellValue($colum . '1', $v);
+            $key += 1;
+        }
+        $column = 2;
+        $objActSheet = $objPHPExcel->getActiveSheet();
+
+        foreach ($data as $key => $rows) { 
+            // 行写入
+            $span = ord("A");
+            foreach ($rows as $keyName => $value) { 
+                // 列写入
+                $j = chr($span);
+                $objActSheet->setCellValue($j . $column, $value);
+                $span ++;
+            }
+            $column ++;
+        }
+        
+        $fileName = iconv("utf-8", "gb2312", $fileName);
+        // 重命名表
+        // 设置活动单指数到第一个表,所以Excel打开这是第一个表
+        $objPHPExcel->setActiveSheetIndex(0);
+        header('Content-Type: application/vnd.ms-excel');
+        header("Content-Disposition: attachment;filename=\"$fileName\"");
+        header('Cache-Control: max-age=0');
+        
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+        $objWriter->save('php://output'); // 文件通过浏览器下载
+        exit();
+    }
+
+    protected function goods_import($filename, $exts = 'xls')
+    {
+        // 导入PHPExcel类库，因为PHPExcel没有用命名空间，只能inport导入
+        vendor('PHPExcel');
+        // 创建PHPExcel对象，注意，不能少了\
+        $PHPExcel = new \PHPExcel();
+        // 如果excel文件后缀名为.xls，导入这个类
+        if ($exts == 'xls') {
+            $PHPReader = new \PHPExcel_Reader_Excel5();
+        } else 
+            if ($exts == 'xlsx') {
+                $PHPReader = new \PHPExcel_Reader_Excel2007();
+            }
+        
+        // 载入文件
+        $PHPExcel = $PHPReader->load($filename);
+        // 获取表中的第一个工作表，如果要获取第二个，把0改为1，依次类推
+        $currentSheet = $PHPExcel->getSheet(0);
+        // 获取总列数
+        $allColumn = $currentSheet->getHighestColumn();
+        // 获取总行数
+        $allRow = $currentSheet->getHighestRow();
+        // 循环获取表中的数据，$currentRow表示当前行，从哪行开始读取数据，索引值从0开始
+        for ($currentRow = 1; $currentRow <= $allRow; $currentRow ++) {
+            // 从哪列开始，A表示第一列
+            for ($currentColumn = 'A'; $currentColumn <= $allColumn; $currentColumn ++) {
+                // 数据坐标
+                $address = $currentColumn . $currentRow;
+                // 读取到的数据，保存到数组$arr中
+                $data[$currentRow][$currentColumn] = $currentSheet->getCell($address)->getValue();
+            }
+        }
+        $this->save_import($data);
     }
 }
