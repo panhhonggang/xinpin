82b20903324eba6ebd7bb336b8a5bc2cf4f5e8c4
chenchangping <chenchangping@foxmail.com>
2017-11-23 11:51:45 +0800

    修改目录权限，后期会修改回来

--- preject/Application/Common/Conf/config.php
+++ preject/Application/Common/Conf/config.php
@@ -5,7 +5,7 @@ return array(
     'DB_HOST'               =>  'localhost', // 服务器地址
     'DB_NAME'               =>  'xinpin',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '',          // 密码
+    'DB_PWD'                =>  'root',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'xp_',    // 数据库表前缀
     'DB_PARAMS'          	=>  array(), // 数据库连接参数
@@ -18,7 +18,7 @@ return array(
     'DB_SLAVE_NO'           =>  '', // 指定从服务器序号
 
     // 显示页面Trace信息
-    'SHOW_PAGE_TRACE' =>true,
+    'SHOW_PAGE_TRACE' =>false,
     /* 权限配置 */
     'AUTH_CONFIG' => array(
         'AUTH_ON' => true, //认证开关
