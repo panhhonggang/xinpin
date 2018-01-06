2d03b55bc36ac14348a58b1a39794c6fddb0ace2
ping <chenchangping>
2018-01-06 16:21:04 +0800

    更新前台统计数据

--- preject/Application/Home/Controller/IndexController.class.php
+++ preject/Application/Home/Controller/IndexController.class.php
@@ -8,8 +8,31 @@ class IndexController extends CommonController
     	
     	// 读取用户余额
     	$money = (int) M('users')->where('id='.$_SESSION['homeuser']['id'])->find()['balance'];
-    	//分配数据        
-        $this->assign('money',$money/100);
+        $map['uid'] = session('homeuser.id');
+        $card = M('card')->where($map)->select();
+        // 拿到IC卡号
+        $iccard = array_column($card,'iccard', 'id');
+
+        // 查询每张IC卡一周的使用记录
+        $icid = array_column($card, 'id');
+        $where['icid'] = ['in', $icid];
+        $times = strtotime('-7 days');
+        $where['time'] = ['egt', $times];
+        $record = M('consume')
+            ->where($where)
+            ->field("did,icid,flow,address,FROM_UNIXTIME(time,'%Y-%m-%d') as time")
+            ->group('time,icid')
+            ->order('time asc')
+            ->select();
+
+    	//分配数据     
+        $assign = [
+            'money' => $money/100,
+            'iccard' => json_encode($res),
+            'record' => json_encode($record),
+        ];   
+        $this->assign($assign);
     	$this->display();
 	}
+
 }
